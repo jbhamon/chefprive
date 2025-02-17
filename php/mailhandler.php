e25f9b4654a11171d395642a9ca217a03c543281
HAMON Jean Boris <bhamon@me.com>
2025-02-17 13:03:02 +0100

    Add php script for main mail address.

---
+++ php/mailhandler.php
@@ -0,0 +1,5 @@
+<?php
+/* Mail handler for the main email address*/
+header ("Location: mailto:petruc.hamon@gmail.com?subject=Demande&nbsp;d'information&nbsp;pour&nbsp;mon&nbsp;repas&nbsp;à&nbspdomicile...");
+exit();
+?>
