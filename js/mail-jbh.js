d814441534ca77fcd36449772ddfba454fc4eda9
jbhamon <bhamon@me.com>
2025-02-18 13:25:45 +0100

    Replace html embedded JavaScript scripts with two JavaScripts files.

---
+++ js/mail-jbh.js
@@ -0,0 +1,8 @@
+document.write(
+  '<a class="footer__copyright" href="mailto:' +
+    "bhamon@" +
+    "me.com" +
+    '?subject=Mail&nbsp;to&nbsp;chefprive.biz&nbsp;webmaster.">' +
+    "Copyright &#169; 2025 JBHamon" +
+    "</a>",
+);
---
+++ js/mail-pch.js
@@ -0,0 +1,8 @@
+emailEpch = "petruc.hamon@" + "gmail.com";
+document.write(
+  '<a href="mailto:' +
+    emailEpch +
+    '?subject=Demande&nbsp;d&#39;information&nbsp;pour&nbsp;mon&nbsp;repas&nbsp;&agrave;&nbsp;domicile...">' +
+    emailEpch +
+    "</a>",
+);
