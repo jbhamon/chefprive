a004d1a9ea43a1da260cfd2ee908e7fb6ae5bdcd
HAMON Jean Boris <bhamon@me.com>
2025-02-21 00:05:18 +0100

    Remove unused code, delete unused files.

--- php/mailhandler.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* Mail handler for the main email address*/
-header ("Location: mailto:petruc.hamon@gmail.com?subject=Demande&nbsp;d'information&nbsp;pour&nbsp;mon&nbsp;repas&nbsp;à&nbspdomicile...");
-exit();
-?>
